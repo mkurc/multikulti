ef8121e29d0e160dc77f5b4bc5f6ff23a09d3696
Michal Jamroz <jamroz@chem.uw.edu.pl>
2014-10-25 23:45:33 +0200

    nic.

--- multikulti/__init__.py
+++ multikulti/__init__.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask#jsonify,request
-from flask.ext.babel import Babel
+from flask import Flask
 
 app = Flask(__name__)
-babel = Babel(app)
-@babel.localeselector
-def get_locale():
-#    return g.get('current_lang', 'en')
-    return 'pl'
 import multikulti.views
 import multikulti.viewsutils
