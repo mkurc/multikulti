179b1ba9e3c9757a437a2317a2c384e31f31b44c
Michal Jamroz <michal@yerbamate.com.pl>
2015-01-08 18:06:12 +0100

    ten dziala na halnym

--- remote_client/optimizeGromacs.sh
+++ remote_client/optimizeGromacs.sh
@@ -15,17 +15,15 @@ lincs-iter = 1000
 lincs-warnangle = 90
 ns_type= simple
 coulombtype= cut-off
-rcoulomb= 0.5
-rvdw= 0.5
 constraints= none
 lincs-order             = 8
 constraint-algorithm = LINCS
 pbc= xyz
 EOF
 
-gmx grompp -f minim.mdp -c conf.gro -p topol.top -o em.tpr -maxwarn 5
-gmx mdrun -s em.tpr -o tr.pdb -c mini.pdb
-echo 2 | gmx trjconv  -f mini.pdb -s em.tpr -o BLBLBL.pdb  -pbc nojump
+grompp -f minim.mdp -c conf.gro -p topol.top -o em.tpr -maxwarn 5
+mdrun -s em.tpr -o tr.pdb -c mini.pdb -nt 1
+echo 2 | trjconv  -f mini.pdb -s em.tpr -o BLBLBL.pdb  -pbc nojump
 mv BLBLBL.pdb $model
 rm mdout.mdp tr.pdb.trr ener.edr mini.pdb topol* posre* conf.gro em.tpr minim.mdp
 mv md.log $model".md.log"
