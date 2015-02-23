5f26db7d7e5f7a486deef705bd5e23def388d994
Michal Jamroz <michal@yerbamate.com.pl>
2015-02-23 13:51:09 +0100

    up lojenie kolejki bug fix

--- remote_client/crontab_check.py
+++ remote_client/crontab_check.py
@@ -6,6 +6,8 @@ import urllib2
 from config_remote import config_remote
 from TalkToServer import TalkToServer
 from sys import exit
+from subprocess import check_output, call
+
 
 runs = os.path.join(config_remote['currdir'],"runJob.sh")
 server = config_remote['webserver_url']+"_queue"
@@ -18,6 +20,9 @@ response = urllib2.urlopen(server)
 for line in response:
     i = line.rstrip()
     command = 'qsub -N "Cdock_%s" %s %s' %(i,runs,i)
-    print command
-    qsub = call(command,shell=True) #TODO
+    check_if_exists="qstat -j Cdock_%s" %(i)
+    ret = call(check_if_exists, shell=True)
+    if ret==1:
+        print command
+        qsub = call(command,shell=True) #TODO
 response.close()
