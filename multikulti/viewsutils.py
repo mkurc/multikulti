0e81c1a5d82fc3ac71aba7ac301b9511407f87f2
Michal Jamroz <michal@yerbamate.com.pl>
2015-03-20 14:47:53 +0100

    benczmark

--- multikulti/viewsutils.py
+++ multikulti/viewsutils.py
@@ -3,7 +3,8 @@
 # Copyright Michal Jamroz, 2014, jamroz@chem.uw.edu.pl
 
 from multikulti import app
-from flask import render_template, g, request, send_from_directory, Response
+from flask import render_template, g, request, send_from_directory, Response, url_for
+from config import config
 import os
 
 
@@ -64,6 +65,53 @@ def learn_more():
 @app.route('/tutorial')
 def tutorial():
     return render_template('_tutorial.html')
+
+
+@app.route('/benchmark')
+def benchmark():
+    jid = {}
+    with open(os.path.join(config['STATIC'],"listaJobow.txt"), "r") as jids:
+        for line in jids:
+            d = line.split()
+            jid[d[0]+d[1]] = d[2:5] + [''] + d[5:]
+            #print jid[d[0]+d[1]]
+
+
+    rows = ""
+    with open(os.path.join(config['STATIC'],"tabelka.txt"), "r") as tab_txt:
+        data = tab_txt.readlines()
+        for line in data:
+            rows += "<tr>" 
+            d = line.split()
+            bound = d[0]
+            unbound = d[1]
+            i = 0
+            j = 0
+            for e in d:
+                if e == "|":
+                    k = bound+unbound
+                    if k in jid:
+                        jd = jid[k]
+                        if len(jd)>j and j!=3:
+                            rows += "<td class=dt-center><a href='%s' target=_blank><i class='fa fa-flask'></i></a></td>" % (url_for('job_status',jid=jd[j]))
+                        elif j>3 and j<7:
+                            rows += "<td class=dt-center><i class='fa fa-close text-danger'></i></td>"
+                    j+=1
+
+                if i < 2 and e !="|":
+                    if e != "-":
+                        rows += "<td class=dt-justify><nobr><a href='http://www.pdb.org/pdb/explore/explore.do?structureId=%s' target='_blank'><i class='fa fa-external-link'></i> %s</a></nobr></td>" % (e,e.upper())
+                    else:
+                        rows += "<td class=dt-center><i class='fa fa-close text-danger'></i></td>"
+                elif e != "|":
+                    if e == '-':
+                        rows += "<td class=dt-right><i class='fa fa-close text-danger'></i></td>"
+                    else:
+                        rows += "<td class=dt-right>%s</td>" % e
+
+                i += 1
+            rows += "</tr>\n"
+    return render_template('benchmark.html', table=rows)
 
 
 @app.route('/job/<jobid>/<filetype>/<filename>')
