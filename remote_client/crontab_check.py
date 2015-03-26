5cedd4110aa5f637294bda86a3c5642e6d7a79f4
Michal Jamroz <jamroz@chem.uw.edu.pl>
2015-03-26 19:24:09 +0100

    que

--- remote_client/crontab_check.py
+++ remote_client/crontab_check.py
@@ -5,21 +5,21 @@ import os
 import urllib2
 from config_remote import config_remote
 from TalkToServer import TalkToServer
-from sys import exit
 
-runs = os.path.join(config_remote['currdir'],"runJob.sh")
+runs = os.path.join(config_remote['currdir'], "runJob.sh")
 server = config_remote['webserver_url']+"_queue"
-environ['SGE_CELL']='default'
-environ['SGE_ROOT']='/var/lib/gridengine'
+environ['SGE_CELL'] = 'default'
+environ['SGE_ROOT'] = '/var/lib/gridengine'
 
 TalkToServer("pingtoserverrrr").IamAlive()
 
 response = urllib2.urlopen(server)
 for line in response:
     i = line.rstrip()
-    command = 'qsub -N "Cdock_%s" %s %s' %(i,runs,i)
-    check_if_exists="qstat -j Cdock_%s" %(i)
+    command = 'qsub -N "Cdock_%s" %s %s' % (i, runs, i)
+    check_if_exists = "qstat -j Cdock_%s" % (i)
     ret = call(check_if_exists, shell=True)
-    if ret==1:
-        qsub = call(command,shell=True)
+    if ret == 1:
+        TalkToServer(i).tellJobWaiting()
+        qsub = call(command, shell=True)
 response.close()
