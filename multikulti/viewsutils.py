f2d680ab21362a83b0b7dcfd65c3c7fb3bd52eb1
Michal Jamroz <jamroz@chem.uw.edu.pl>
2015-04-09 17:06:45 +0200

    benchmark up

--- multikulti/viewsutils.py
+++ multikulti/viewsutils.py
@@ -81,10 +81,10 @@ def benchmark():
             jid[d[0]+d[1]] = d[2:5] + [''] + d[5:]
 
     rows = ""
-    c = "<td class=dt-center><a href='%s' target=_blank>\
+    c = "<td class='sm dt-center'><a href='%s' target=_blank>\
             <i class='fa fa-flask'></i></a></td>\n"
-    c2 = "<td class=dt-center><i class='fa fa-close text-danger'></i></td>\n"
-    c3 = "<td class=dt-justify><nobr><a href='http://www.pdb.org/pdb/explore/explore.do?structureId=%s' \
+    c2 = "<td class='dt-center sm'><i class='fa fa-close text-danger'></i></td>\n"
+    c3 = "<td class='sm dt-justify'><nobr><a href='http://www.pdb.org/pdb/explore/explore.do?structureId=%s' \
             target='_blank'><i class='fa fa-external-link'></i> %s</a></nobr></td>\n"
 
     with open(os.path.join(config['STATIC'], "tabelka.txt"), "r") as tab_txt:
@@ -114,12 +114,12 @@ def benchmark():
                     if e != "-":
                         rows += c3 % (e, e.upper())
                     else:
-                        rows += "<td class=dt-center><i class='fa fa-close text-danger'></i></td>\n"
+                        rows += "<td class='sm dt-center'><i class='fa fa-close text-danger'></i></td>\n"
                 elif e != "|":
                     if e == '-':
-                        rows += "<td class=dt-center><i class='fa fa-close text-danger'></i></td>\n"
+                        rows += "<td class='dt-center sm'><i class='fa fa-close text-danger'></i></td>\n"
                     else:
-                        rows += "<td data-val='%s' data-num='%s' class=dt-right>%s</td>\n" % (jjid, lab[kk], e)
+                        rows += "<td data-val='%s' data-num='%s' class='sm dt-right'>%s</td>\n" % (jjid, lab[kk], e)
                         kk += 1
                         if kk > 3:
                             kk = 0
