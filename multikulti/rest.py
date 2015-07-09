044313b8909bdb3fec32c94a96ed7f9877444a92
Konrad <konrad.kozak@biarchitects.pl>
2015-07-09 16:08:51 +0200

    rest_poprawki

--- multikulti/rest.py
+++ multikulti/rest.py
@@ -17,7 +17,7 @@ from views import gunzip, unique_id, cluster_stats, alphanum_key
 from glob import glob
 
 # tylko status
-@app.route('/REST/status/<string:job_id>', methods=['GET', 'POST'])
+@app.route('/REST/status/<string:job_id>', methods=['GET'])
 def status(job_id):
     todel = str(app.config['DELETE_USER_JOBS_AFTER'])
     if request.method == 'POST' and request.json:
@@ -25,7 +25,7 @@ def status(job_id):
         system_info = query_db("SELECT ligand_sequence, receptor_sequence, \
         status_date, date_add(status_date, interval  %s day) del, \
         ligand_chain, status_init status_change, project_name, status, \
-        constraints_scaling_factor, ligand_ss, ss_psipred FROM user_queue \
+        ligand_ss, ss_psipred FROM user_queue \
         WHERE jid=%s", [todel, id], one=True)
 
         if not system_info:
@@ -41,7 +41,7 @@ def status(job_id):
 
 
 # wszystko z user_quere
-@app.route('/REST/job_info/<string:job_id>', methods=['GET', 'POST'])
+@app.route('/REST/job_info/<string:job_id>', methods=['GET'])
 def job_info(job_id):
     todel = str(app.config['DELETE_USER_JOBS_AFTER'])
     if request.method == 'POST' and request.json:
@@ -49,7 +49,7 @@ def job_info(job_id):
         system_info = query_db("SELECT ligand_sequence, receptor_sequence, \
         status_date, date_add(status_date, interval  %s day) del, \
         ligand_chain, status_init status_change, project_name, status, \
-        constraints_scaling_factor, ligand_ss, ss_psipred FROM user_queue \
+        ligand_ss, ss_psipred FROM user_queue \
         WHERE jid=%s", [todel, id], one=True)
 
         flexible = query_db("SELECT constraint_definition FROM constraints WHERE jid=%s", [id])
@@ -70,15 +70,14 @@ def job_info(job_id):
 
 
 # pobranie pelnych wynikow
-# proponowalbym 3 typy pobran - min, medium, full rozniace sie iloscia danych
-@app.route('/REST/job_results/<string:jid>', methods=['GET', 'POST'])
-def get_job(jid):
+@app.route('/REST/job_results_all/<string:jid>', methods=['GET', 'POST'])
+def get_job_all(jid):
     todel = str(app.config['DELETE_USER_JOBS_AFTER'])
 
     system_info = query_db("SELECT ligand_sequence, receptor_sequence, \
         status_date, date_add(status_date, interval  %s day) del, \
         ligand_chain, status_init status_change, project_name, status, \
-        constraints_scaling_factor, ligand_ss, ss_psipred FROM user_queue \
+        ligand_ss, ss_psipred FROM user_queue \
         WHERE jid=%s", [todel, jid], one=True)
 
     if not system_info:
@@ -89,7 +88,6 @@ def get_job(jid):
     udir_path = os.path.join(app.config['USERJOB_DIRECTORY'], jid)
 
     clust_details = cluster_stats(jid)
-    # print clust_details
 
     for d in ['models', 'replicas', 'clusters']:
         tm = [fil.split("/")[-1] for fil in glob(udir_path + "/" + d + "/*.gz")]
@@ -128,7 +126,7 @@ def get_job(jid):
             'model': i + 1,
             'model_data': file_content_model,
             'cluster_data': file_content_cluster,
-            'replica_data': file_content_replica,
+            'trajectory_data': file_content_replica,
             'cluster_density': clust_details[i]["density"],
             'average_rmsd': clust_details[i]["rmsd"],
             'max_rmsd': clust_details[i]["maxrmsd"],
@@ -136,6 +134,113 @@ def get_job(jid):
         })
 
     result = {'models': results}
+
+    return jsonify(result)
+
+
+@app.route('/REST/job_results/<string:jid>', methods=['GET', 'POST'])
+def get_job(jid):
+
+
+    todel = str(app.config['DELETE_USER_JOBS_AFTER'])
+
+    system_info = query_db("SELECT ligand_sequence, receptor_sequence, \
+            status_date, date_add(status_date, interval  %s day) del, \
+            ligand_chain, status_init status_change, project_name, status, \
+            ligand_ss, ss_psipred FROM user_queue \
+            WHERE jid=%s", [todel, jid], one=True)
+
+    if not system_info:
+        return jsonify({
+            'status': 'error'})
+
+    models = {'models': [], 'clusters': [], 'replicas': []}
+    udir_path = os.path.join(app.config['USERJOB_DIRECTORY'], jid)
+
+    clust_details = cluster_stats(jid)
+
+    for d in ['models']:
+        tm = [fil.split("/")[-1] for fil in glob(udir_path + "/" + d + "/*.gz")]
+        models[d] = sorted(tm, key=alphanum_key)
+
+    range_list = xrange(0, 10)
+    if request.method == 'POST' and request.json:
+        value = request.json.get('filter')  # po cluster_stats
+        min = request.json.get('min')
+        max = request.json.get('max')
+        if value and min and max:
+            filtered_results = filter(lambda x: float(x[value]) > float(min) and float(x[value]) <= float(max),
+                                      clust_details)
+            range_list = map(lambda x: x['id'], filtered_results)
+
+    results = []
+    for i in range_list:
+        path_dir = os.path.join(app.config['USERJOB_DIRECTORY'], jid,
+                                "models", models['models'][i])
+        with gzip.open(path_dir) as data:
+            file_content_model = data.read()
+
+        results.append({
+            'jobid': jid,
+            'info': system_info,
+            'model': i + 1,
+            'model_data': file_content_model,
+            'cluster_density': clust_details[i]["density"],
+            'average_rmsd': clust_details[i]["rmsd"],
+            'max_rmsd': clust_details[i]["maxrmsd"],
+            'elements': clust_details[i]["counts"]
+        })
+
+    result = {'models': results}
+
+    return jsonify(result)
+
+
+@app.route('/REST/get_cluster/<string:jid>/<string:model_id>', methods=['GET', 'POST'])
+def get_cluster(jid,model_id):
+
+    model_id = int(model_id)
+    clust_details = cluster_stats(jid)
+
+    cluster_name = "cluster_%s.pdb.gz" %model_id
+    path_dir = os.path.join(app.config['USERJOB_DIRECTORY'], jid,
+                            "clusters", cluster_name)
+    with gzip.open(path_dir) as data:
+        file_content_cluster = data.read()
+
+    results = []
+    results.append({
+        'jobid': jid,
+        'cluster_data': file_content_cluster,
+        'cluster_density': clust_details[model_id]["density"],
+        'average_rmsd': clust_details[model_id]["rmsd"],
+        'max_rmsd': clust_details[model_id]["maxrmsd"],
+        'elements': clust_details[model_id]["counts"]
+    })
+
+    result = {'model': results}
+
+    return jsonify(result)
+
+#dziala okej przez curla
+@app.route('/REST/get_trajectory/<string:jid>/<string:trajectory_id>', methods=['GET', 'POST'])
+def get_trajectory(jid,trajectory_id):
+
+    trajectory_id = int(trajectory_id)
+    trajectory_name = "replica_%s.pdb.gz" %trajectory_id
+
+    path_dir = os.path.join(app.config['USERJOB_DIRECTORY'], jid,
+                            "replicas", trajectory_name)
+    with gzip.open(path_dir) as data:
+        file_content_replica = data.read()
+
+    results = []
+    results.append({
+        'jobid': jid,
+        'trajectory': file_content_replica,
+    })
+
+    result = {'model': results}
 
     return jsonify(result)
 
@@ -194,19 +299,62 @@ def prepare_data(data, receptor_file):
     data['simulation_cycles'] = int(data.get('simulation_cycles', 50))
     data['project_name'] = data.get('project_name', '')
     data['email'] = data.get('email', '')
-    data['show'] = data.get('show_job', True)
+    data['show'] = json_bool_to_db(data.get('show_job'))
     data['excluded_regions'] = data.get('excluded_regions', [])
     data['constraints'] = data.get('constraints', [])
     data['scaling_factor'] = data.get('overall_weight', '1.0')
+
     return data
+
+
+def check_range(file, constraints, excluded):
+    values_by_chain = {}
+    for match in re.findall(r'ATOM\s+\d+\s+\w+\s+\w+\s+([A-Z])\s+(\d+)', file.read()):
+        chain = match[0]
+        val = int(match[1])
+        if chain not in values_by_chain:
+            values_by_chain[chain] = [val, val]
+        else:
+            values_by_chain[chain][0] = min(values_by_chain[chain][0], val)
+            values_by_chain[chain][1] = max(values_by_chain[chain][1], val)
+
+    for i in constraints:
+        if i['chain'] not in values_by_chain:
+            raise RestValidationError('Selected chain (%s) not exist' % i["chain"])
+        if int(i['start']) < values_by_chain[i['chain']][0] or int(i['end']) > values_by_chain[i['chain']][1]:
+            raise RestValidationError('Selected region out of receptor range for chain %s' % i['chain'])
+
+    for i in excluded:
+        if i['chain'] not in values_by_chain:
+            raise RestValidationError('Selected chain (%s) not exist' % i["chain"])
+        if int(i['start']) < values_by_chain[i['chain']][0] or int(i['end']) > values_by_chain[i['chain']][1]:
+            raise RestValidationError('Selected region out of receptor for chain %s' % i['chain'])
+
+    return True
+
+
+def json_bool_to_db(data):
+    if isinstance(data, bool):
+        return data
+    if data is None:
+        return 0
+    if str(data).lower() == "true":
+        return 1
+    else:
+        return 0
 
 
 def add_data_to_db(data):
     jid = unique_id()
 
+    if data['receptor_file'] != None:
+        pdb = get_PDB_from_file(jid, data['receptor_file'])
+    else:
+        pdb = get_PDB_file(data['receptor_pdb_code'], jid)
+
     return (query_db("INSERT INTO user_queue(jid, email, receptor_sequence, \
          ligand_sequence, ligand_ss, hide, project_name,simulation_length) \
-         VALUES(%s,%s,%s,%s,%s,%s,%s,%s)", [jid, data['email'], get_PDB_file(data['receptor_pdb_code'], jid),
+         VALUES(%s,%s,%s,%s,%s,%s,%s,%s)", [jid, data['email'], pdb,
                                             data['ligand_seq'], data['ligand_ss'], data['show'], data['project_name'],
                                             data['simulation_cycles']], insert=True), jid)
 
@@ -237,6 +385,22 @@ def get_PDB_file(pdb_code, jid):
     return receptor_seq
 
 
+def get_PDB_from_file(jid, file):
+    dest_directory = os.path.join(app.config['USERJOB_DIRECTORY'], jid)
+    dest_file = os.path.join(dest_directory, "input.pdb.gz")
+    os.mkdir(dest_directory)
+
+    p = PdbParser(file)
+    receptor_seq = p.getSequence()
+    p.savePdbFile(dest_file)
+    gunzip(dest_file)
+
+    if len(receptor_seq) > 500:
+        raise RestValidationError('Max protein size: 500 residues')
+
+    return receptor_seq
+
+
 def add_excluded(data, jid):
     for i in data['excluded_regions']:
         normal = i['start'] + ':' + i['chain'] + ' ' + '-' + ' ' + i['end'] + ':' + i['chain']
@@ -261,6 +425,7 @@ def user_add_constraints(data, jid):
 
 
 def validate_data(data, receptor_file):
+    check_range(receptor_file, data['constraints'], data['excluded_regions'])
     validate_name(data.get('project_name'))
     validate_receptor_file(receptor_file)
     validate_pdb_input(data.get('receptor_pdb_code'), receptor_file)
