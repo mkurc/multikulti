07b124a036d26c1e9b68a0b55ee974b1343bc999
Konrad <konrad.kozak@biarchitects.pl>
2015-09-08 06:28:25 +0200

    poprawka ze zmiana statusu tak aby wchodził do kolejki obliczeniowej

--- multikulti/rest.py
+++ multikulti/rest.py
@@ -283,6 +283,7 @@ def get_selected_trajectory(jid, model, start, end):
 @app.route('/REST/add_job/', methods=['POST'])
 def add_job():
     if request.method == 'POST':
+        #print request.form.keys()
         receptor_file = request.files.get('file')
         try:
             if request.json is not None:
@@ -361,10 +362,10 @@ def add_data_to_db(data):
 
     pdb = read_sequence_from_content_and_save(data['file_content'], jid)
     return (query_db("INSERT INTO user_queue(jid, email, receptor_sequence, \
-         ligand_sequence, ligand_ss, hide, project_name,simulation_length) \
+         ligand_sequence, ligand_ss, hide, project_name,simulation_length,status) \
          VALUES(%s,%s,%s,%s,%s,%s,%s,%s)", [jid, data['email'], pdb,
                                             data['ligand_seq'], data['ligand_ss'], data['show'], data['project_name'],
-                                            data['simulation_cycles']], insert=True), jid)
+                                            data['simulation_cycles'],'pre_queue'], insert=True), jid)
 
 
 def read_sequence_from_content_and_save(content, jid):
